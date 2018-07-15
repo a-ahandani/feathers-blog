e1edcab1b58a34bcbab0638d1673d9824c140311
Ahmad Esmaeilzadeh <esmaeilzade.ahmad@gmail.com>
2018-07-15 18:08:09 +0430

    Categories service added.

--- src/services/index.js
+++ src/services/index.js
@@ -4,6 +4,7 @@ const comments = require('./comments/comments.service.js');
 const makers = require('./makers/makers.service.js');
 const tags = require('./tags/tags.service.js');
 const toTags = require('./to-tags/to-tags.service.js');
+const categories = require('./categories/categories.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(posts);
@@ -12,4 +13,5 @@ module.exports = function (app) {
   app.configure(makers);
   app.configure(tags);
   app.configure(toTags);
+  app.configure(categories);
 };
