416601495f5ca9d5fbdc2ebc03bc949e085d42c6
Unknown <esmaeilzade.ahmad@gmail.com>
2018-07-23 21:20:17 +0430

    Database migration added.

--- migrations/config/config.js
+++ migrations/config/config.js
@@ -1,6 +1,6 @@
 const app = require('../../src/app');
 const env = process.env.NODE_ENV || 'development';
-const dialect = 'mysql'|'sqlite'|'postgres'|'mssql';
+const dialect = 'postgres';
 
 module.exports = {
   [env]: {
